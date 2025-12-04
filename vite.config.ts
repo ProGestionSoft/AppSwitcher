ef4f4bcb8fe88d3f4e231847554c136eb08a32d8
SteveAsterAfovo <steveasterafovo@gmail.com>
2025-12-04 01:43:51 +0100

    correction cdn action on prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,45 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/standalone/index.ts'),
-      name: 'PGSAppSwitcher',
-      formats: ['es', 'umd', 'iife'],
-      fileName: (format) => {
-        if (format === 'es') return 'app-switcher.es.js'
-        if (format === 'umd') return 'app-switcher.umd.js'
-        return 'app-switcher.iife.js'
-      }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue()],
+    define: {
+      'import.meta.env.PGS_API_URL': JSON.stringify(env.PGS_API_URL)
     },
-    rollupOptions: {
-      external: [],
-      output: {
-        globals: {},
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name === 'style.css') return 'app-switcher.css'
-          return assetInfo.name || ''
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/standalone/index.ts'),
+        name: 'PGSAppSwitcher',
+        formats: ['es', 'umd', 'iife'],
+        fileName: (format) => {
+          if (format === 'es') return 'app-switcher.es.js'
+          if (format === 'umd') return 'app-switcher.umd.js'
+          return 'app-switcher.iife.js'
         }
-      }
+      },
+      rollupOptions: {
+        external: [],
+        output: {
+          globals: {},
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name === 'style.css') return 'app-switcher.css'
+            return assetInfo.name || ''
+          }
+        }
+      },
+      outDir: 'dist',
+      emptyOutDir: true
     },
-    outDir: 'dist',
-    emptyOutDir: true
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-      '~': resolve(__dirname, '.')
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+        '~': resolve(__dirname, '.')
+      }
     }
   }
 })
